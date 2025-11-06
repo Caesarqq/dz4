48f0065991e66aaf8399a0b317f6e432eeae4501
Caesarqq <aszxclkh@gmail.com>
2025-11-06 12:32:29 +0300

    фикс6

--- main.go
+++ main.go
@@ -14,51 +14,61 @@ func main() {
 	opts, inputFile, outputFile, err := parseArgs(os.Args[1:])
 	if err != nil {
 		fmt.Println(err)
-		fmt.Println("Usage: uniq [-c | -d | -u] [-i] [-f num] [-s chars] [input_file [output_file]]")
+		printUsage()
 		return
 	}
 
+	lines, err := getLines(inputFile)
+	if err != nil {
+		fmt.Println("Ошибка при чтении входного файла:", err)
+		return
+	}
+
+	result := uniq.ProcessLines(lines, opts)
+
+	if err := writeOutput(outputFile, result); err != nil {
+		fmt.Println("Ошибка при записи в выходной файл:", err)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Использование: uniq [-c | -d | -u] [-i] [-f число] [-s символы] [входной_файл [выходной_файл]]")
+}
+
+func getLines(inputFile string) ([]string, error) {
 	var reader io.Reader
 	if inputFile != "" {
 		file, err := os.Open(inputFile)
 		if err != nil {
-			fmt.Println("Error opening input file:", err)
-			return
+			return nil, err
 		}
 		defer file.Close()
 		reader = file
 	} else {
 		reader = os.Stdin
 	}
+	return readLines(reader)
+}
 
-	lines, err := readLines(reader)
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-
-	result := uniq.ProcessLines(lines, opts)
+func writeOutput(outputFile string, lines []string) error {
 	var writer io.Writer
 	if outputFile != "" {
 		file, err := os.Create(outputFile)
 		if err != nil {
-			fmt.Println("Error creating output file:", err)
-			return
+			return err
 		}
 		defer file.Close()
 		writer = file
 	} else {
 		writer = os.Stdout
 	}
-
-	writeLines(writer, result)
+	return writeLines(writer, lines)
 }
 
 func parseArgs(args []string) (uniq.Options, string, string, error) {
 	var opts uniq.Options
 	var inputFile, outputFile string
 	i := 0
-
 	for i < len(args) {
 		arg := args[i]
 		switch arg {
@@ -72,21 +82,21 @@ func parseArgs(args []string) (uniq.Options, string, string, error) {
 			opts.IgnoreCase = true
 		case "-f":
 			if i+1 >= len(args) {
-				return opts, "", "", fmt.Errorf("missing value for -f")
+				return opts, "", "", fmt.Errorf("отсутствует значение для -f")
 			}
 			val, err := strconv.Atoi(args[i+1])
 			if err != nil {
-				return opts, "", "", fmt.Errorf("invalid -f value: %v", args[i+1])
+				return opts, "", "", fmt.Errorf("неверное значение для -f: %v", args[i+1])
 			}
 			opts.SkipFields = val
 			i++
 		case "-s":
 			if i+1 >= len(args) {
-				return opts, "", "", fmt.Errorf("missing value for -s")
+				return opts, "", "", fmt.Errorf("отсутствует значение для -s")
 			}
 			val, err := strconv.Atoi(args[i+1])
 			if err != nil {
-				return opts, "", "", fmt.Errorf("invalid -s value: %v", args[i+1])
+				return opts, "", "", fmt.Errorf("неверное значение для -s: %v", args[i+1])
 			}
 			opts.SkipChars = val
 			i++
@@ -111,7 +121,7 @@ func parseArgs(args []string) (uniq.Options, string, string, error) {
 		flagCount++
 	}
 	if flagCount > 1 {
-		return opts, "", "", fmt.Errorf("Error: can't use -c, -d, -u together")
+		return opts, "", "", fmt.Errorf("Ошибка: флаги -c, -d и -u нельзя использовать одновременно")
 	}
 
 	return opts, inputFile, outputFile, nil
@@ -129,7 +139,9 @@ func readLines(r io.Reader) ([]string, error) {
 func writeLines(w io.Writer, lines []string) error {
 	writer := bufio.NewWriter(w)
 	for _, line := range lines {
-		fmt.Fprintln(writer, line)
+		if _, err := fmt.Fprintln(writer, line); err != nil {
+			return err
+		}
 	}
 	return writer.Flush()
 }
